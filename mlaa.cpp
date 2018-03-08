8022ae5f2ce2bac7d16f9a0ddff9b58f23a2f848
Vegard Stenhjem Hagen <vegard.hagen@ntnu.no>
2018-03-08 17:09:18 +0100

    Cleanup and refactoring of code.

--- mlaa.cpp
+++ mlaa.cpp
@@ -62,37 +62,7 @@ void writeImg(const int *grey, const int w, const int h, const char* fname) {
     writeImg(grey, grey, grey, w, h, fname);
 }
 
-void edgeDetect(const int w, const int h, const int *pix, int *ver, int *hor) {
-    const int LIMIT = 64;
-
-    int ind, ind_r, ind_b;
-
-    // Find vertical edges. Scan horizontal.
-    for (int i=0; i<w-1; i++) {
-        for (int j=0; j<h; j++) {
-            ind   = idx(w,h,i  ,j  );
-            ind_r = idx(w,h,i+1,j  );
-            if (abs(pix[ind] - pix[ind_r]) > LIMIT) {
-                // Pick the pixel with the highest value
-                ver[(pix[ind]>pix[ind_r])?(ind):(ind_r)] = 1;
-            }
-        }
-    }
-
-    // Find horizontal edges. Scan vertical.
-    for (int i=0; i<w; i++) {
-        for (int j=0; j<h-1; j++) {
-            ind   = idx(w,h,i  ,j  );
-            ind_b = idx(w,h,i  ,j+1);
-            if (abs(pix[ind] - pix[ind_b]) > LIMIT) {
-                // Pick the pixel with the highest value
-                hor[(pix[ind]>pix[ind_b])?(ind):(ind_b)] = 1;
-            }
-        }
-    }
-}
-
-void edgeDetect2(const int w, const int h, const int *pix, int *edge) {
+void edgeDetect(const int w, const int h, const int *pix, int *edge) {
     const int LIMIT = 64;
 
     int ind, ind_r, ind_b;
@@ -124,14 +94,7 @@ void edgeDetect2(const int w, const int h, const int *pix, int *edge) {
     }
 }
 
-struct line {
-    int beg_x;
-    int beg_y;
-    int end_x;
-    int end_y;
-};
-
-class Shape2 {
+class Shape {
     public:
         void setStart(int _x, int _y) {start_x = _x; start_y = _y;}
         void setEnd(int _x, int _y) {end_x = _x; end_y = _y;}
@@ -151,6 +114,10 @@ class Shape2 {
 
         int getType(){return type;}
 
+        float getLength(){
+            return sqrt((start_x-end_x)*(start_x-end_x) + (start_y-end_y)*(start_y-end_y));
+        }
+
         void reset(){
             start_x = -1;
             start_y = -1;
@@ -168,38 +135,25 @@ class Shape2 {
 };
 
 
-void findShapes2(const int w, const int h, int *edge) {
-
-    std::vector<Shape2> shapes;
-
+void findShapes(const int w, const int h, int *edge, std::vector<Shape> &shapes) {
     enum {l_edge=1, r_edge=2, b_edge=4, t_edge=8};
-
-    //int v_edge = (l_edge|r_edge);
-    //int h_edge = (b_edge|t_edge);
-
 
     int v_edge, h_edge;
 
     int ind,ind_b;
-    int slice;
-    Shape2 shape;
+    Shape shape;
 
-    bool found_left  = false;
-    bool found_right = false;
-    bool found_hor   = false;
+    bool found_right;
+    bool found_left;
+    bool found_hor ;
 
     int *ort = new int[2];
-    ort[0] = -1;
-    ort[1] = -1;
-    int start = -1;
+    //ort[0] = -1; ort[1] = -1;
 
     for (int j=0; j<h-1; j++) {
         for (int i=0; i<w; i++) {
             ind   = idx(w,h,i  ,j  );
             ind_b = idx(w,h,i  ,j+1);
-
-          //  std::cout << std::setw(2) << edge[ind]   << " ";
-          //  std::cout << std::setw(2) << edge[ind_b] << std::endl;
 
             // Create slices that are the collective of the upper and lower pixel being checked.
             v_edge = ((edge[ind] | edge[ind_b]) & (l_edge | r_edge));
@@ -207,8 +161,8 @@ void findShapes2(const int w, const int h, int *edge) {
 
             // Reset flags for each line
             if (i==0) {
-                found_left  = false;
                 found_right = false;
+                found_left  = false;
                 found_hor   = false;
                 ort[0] = -1;
                 ort[1] = -1;
@@ -220,7 +174,6 @@ void findShapes2(const int w, const int h, int *edge) {
             }
 
             // True if we have a vertical edge and haven't found a left edge yet.
-
             if (v_edge && !found_left) {
                 ort[0] = i; //(v_edge & r_edge);
                 ort[1] = (edge[ind] & v_edge)?(j):(j+1);
@@ -256,122 +209,49 @@ void findShapes2(const int w, const int h, int *edge) {
 
                 shapes.push_back(shape);
 
-                // reset right and horizontal edge
+                // Reset flags
                 found_right = false;
+                found_left  = false;
                 found_hor   = false;
                 shape.reset();
-           //     // reset shape
-           //     shape.start_x = -1;
-           //     shape.start_y = -1;
-           //     shape.end_x   = -1;
-           //     shape.end_y   = -1;
-           //     shape.type    = -1;
-            }
-        }
-    }
-
-
-    int *pix = new int[w*h];
-
-    for (int i=0; i<w*h; i++) {pix[i] = 255;}
-
-    for (int i=0; i<shapes.size(); i++){
-        shape = shapes.at(i);
-        std::cout << std::setw(2) << shape.getStartX() << " " << std::setw(2) << shape.getStartY() << " -> ";
-        std::cout << std::setw(2) << shape.getEndX()   << " " << std::setw(2) << shape.getEndY()   << " \t ";
-        std::cout << std::setw(2) << shape.getType()    << std::endl;
-        pix[idx(w,h,shape.getStartX(), shape.getStartY())] = 0;
-        pix[idx(w,h,shape.getEndX()  , shape.getEndY()  )] = 0;
-    }
-    writeImg(pix, w, h, "edge2.bmp");
-}
-
-void blend(const int left, const int right, int *pix) {
-
-}
-
-struct Shape {
-    int start_x = -1;
-    int start_y = -1;
-    int stop_x  = -1;
-    int stop_y  = -1;
-    int turns   =  0; // max two turns
-    int type    = -1;
-//    int dir   = -1; // last direction [n,e,s,w] = [0,1,2,3];
-};
-
-void checkNbd(const int w, const int h, int i, int j, int *edge, int *checked, Shape *shape){
-    // Check neighbourhooding pixels if they contain an edge and expand shape if criteria are met
-    int nbd;
-    int k[8] = { 0, 1, 1, 1, 0,-1,-1,-1};
-    int l[8] = { 1, 1, 0,-1,-1,-1, 0, 1};
-
-    //  7 0 1
-    //  6 X 2
-    //  5 4 3
-
-    int t_norm;
-
-    for (int n=0; n<8; n++) {
-        // Stop if we have found our shape
-        if (shape->turns > 1) {return;}
-        nbd = idx(w,h,i+k[n],j+l[n]);
-        if ((nbd < w*h) && (nbd > 0)) {
-            // Skip if we're already checked pixel
-            if (checked[nbd]) {continue;}
-            checked[nbd] = 100;
-            if (edge[nbd]) {
-                t_norm = abs(k[n]) + abs(l[n]);
-
-                if ((shape->turns > 0) || (t_norm == 2)) checked[nbd] = 0;
-
-                // Continue expanding shape if we can
-                if (t_norm < 2) {
-                    shape->stop_x = i+k[n];
-                    shape->stop_y = j+l[n];
-                    checkNbd(w,h,i+k[n],j+l[n],edge,checked,shape);
-                }
-                if ((t_norm == 2) && shape->turns < 1) {
-                    shape->stop_x = i+k[n];
-                    shape->stop_y = j+l[n];
-                    shape->turns++;
-                    checkNbd(w,h,i+k[n],j+l[n],edge,checked,shape);
-                }
-                return;
             }
         }
     }
 }
 
-void findShapes(const int w, const int h, int *edge, std::vector<Shape> &shapes) {
-    int ind;
+void blend(const int w, const int h, int *pix, Shape shape) {
+    int c_old, c_opp;
+    float b1, b2;
+    float A;
 
-    int *checked = new int[w*h];
-    for (int i=0; i<w*h; i++) checked[i] = 0;
+    int *tmp = new int[w*h];
+    for (int i=0; i<w*h; i++) {tmp[i] = pix[i];}
 
-    for (int i=0; i<w; i++) {
-        for (int j=h-1; j>-1; j--) {
-            ind = idx(w,h,i,j);
-            if (edge[ind] && !checked[ind]) {
-                checked[ind] = 10;
-                shapes.push_back(Shape());
-                shapes.back().start_x = i;
-                shapes.back().start_y = j;
-                // Check neigbourhood
-                checkNbd(w,h,i,j,edge,checked,&shapes.back());
-                // The last shape part should be available for next shape
-                // checked[idx(w,h,shapes.back().stop_x,shapes.back().stop_y)] = 0;
+    if (shape.getType() == 2){
+        int x = shape.getEndX() - shape.getStartX();
+        int y = shape.getStartY();
+        for (int i=0; i<x; i++) {
+            b1 = 0.5 - (i    )/(1.0*x);
+            b2 = 0.5 - (i+1.0)/(1.0*x);
+            // If we're in a split cell only calculate the triangle area,
+            // else the area will be zero.
+            A = 0.5*fabs((fabs(b1+b2)>1e-8)?(b1+b2):(b1));
+            if (b1 > 0){
+                c_old = tmp[idx(w,h,shape.getStartX()+(i+1),shape.getStartY())];
+                c_opp = tmp[idx(w,h,shape.getStartX()+(i+1),shape.getEndY()  )];
+                pix[idx(w,h,shape.getStartX()+(i+1),shape.getStartY())] = (1-A)*c_old + A*c_opp;
+            }
+            if (b2 < 0){
+                c_old = tmp[idx(w,h,shape.getStartX()+(i+1),shape.getEndY()  )];
+                c_opp = tmp[idx(w,h,shape.getStartX()+(i+1),shape.getStartY())];
+                pix[idx(w,h,shape.getStartX()+(i+1),shape.getEndY())  ] = (1-A)*c_old + A*c_opp;
             }
         }
     }
+    delete[] tmp;
 }
 
 int main (int argc, char *argv[]) {
-
-    enum {l_edge=1, r_edge=2, b_edge=4, t_edge=8};
-
-//    int test = l_edge + t_edge;
-//    std::cout << (test & l_edge) << " " << (test & b_edge) << " " << (test & t_edge) << std::endl;
 
     int w =   20;
     int h =   10;
@@ -413,22 +293,10 @@ int main (int argc, char *argv[]) {
 
     writeImg(pix,w,h,"raw.bmp");
 
-    int *ver = new int[w*h];
-    int *hor = new int[w*h];
-
-    for (int i=0; i<w*h; i++) {
-        ver[i] = 255;
-        hor[i] = 255;
-    }
-
-    edgeDetect(w, h, pix, ver, hor);
-    writeImg(ver,w,h,"0ver.bmp");
-    writeImg(hor,w,h,"0hor.bmp");
-
     int *edge = new int[w*h];
     for (int i=0; i<w*h; i++) edge[i] = 0;
 
-    edgeDetect2(w, h, pix, edge);
+    edgeDetect(w, h, pix, edge);
 
     int *edgepix = new int[w*h];
     for (int i=0; i<w; i++) {
@@ -438,9 +306,24 @@ int main (int argc, char *argv[]) {
             if (edge[ind]) edgepix[ind] = edge[ind];
         }
     }
-
     writeImg(edgepix,w,h,"edge.bmp");
-    findShapes2(w, h, edge);
+
+    std::vector<Shape> shapes;
+    findShapes(w, h, edge, shapes);
+
+    int *shapepix = new int[w*h];
+    for (int i=0; i<w*h; i++) {shapepix[i] = 255;}
+
+    Shape shape;
+    for (int i=0; i<shapes.size(); i++){
+        shape = shapes.at(i);
+        std::cout << std::setw(2) << shape.getStartX() << " " << std::setw(2) << shape.getStartY() << " -> ";
+        std::cout << std::setw(2) << shape.getEndX()   << " " << std::setw(2) << shape.getEndY()   << " \t ";
+        std::cout << std::setw(2) << shape.getType()    << std::endl;
+        shapepix[idx(w,h,shape.getStartX(), shape.getStartY())] = 0;
+        pix[idx(w,h,shape.getEndX()  , shape.getEndY()  )] = 0;
+    }
+    writeImg(shapepix, w, h, "shapes.bmp");
 
     std::cout << std::endl << "   ";
     for (int i=0; i<w; i++) std::cout << std::setw(2) << i << " ";
@@ -456,35 +339,11 @@ int main (int argc, char *argv[]) {
         }
     }
     std::cout << std::endl;
-//    std::vector<Shape> shapes;
-//
-//    findShapes(w, h, edge, shapes);
-//
-//    std::cout << "Number of shapes: " << shapes.size() << std::endl;
 
-    float len;
-    Shape shape;
-
-//    for (int i=0; i<shapes.size(); i++){
-//        shape = shapes.at(i);
-//        std::cout << shape.start_x << " " << shape.start_y << " -> ";
-//        std::cout << shape.stop_x  << " " << shape.stop_y  << " \t ";
-//        std::cout << shape.turns   << std::endl;
-//
-//     //   len = sqrt((shape.start_x-shape.stop_x)*(shape.start_x-shape.stop_x) + (shape.start_x-shape.stop_x)*(shape.start_x-shape.stop_x));
-//     //   std::cout << len << std::endl;
-//
-//        for (int i=shape.start_x; i<shape.stop_x; i++) {
-//            for (int j=shape.stop_y; j<shape.start_y; j++) {
-//                pix[idx(w,h,i,j)] = 0.5*(pix[idx(w,h,i-1,j)] + pix[idx(w,h,i+1,j)]);
-//
-//            }
-//        }
-//
-//    }
-
-    writeImg(pix,w,h,"aa.bmp");
-//    writeImg(checked,w,h,"checked.bmp");
+    for (int i=0; i<shapes.size(); i++){
+        blend(w,h,pix,shapes.at(i));
+    }
+    writeImg(pix,w,h,"mlaa.bmp");
 
     return 0;
 }
